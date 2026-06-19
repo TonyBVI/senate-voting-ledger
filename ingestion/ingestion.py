f11c3acb5dc5aac0a12fb62d33a79baf8e629a5f
TonyBVI <TonyBVI@ybvi.com>
2026-06-19 09:49:19 +0200

    Create ingestion.py

---
+++ ingestion/ingestion.py
@@ -0,0 +1,17 @@
+
+import requests
+
+def get_latest_vote_number():
+    # TODO: implement Senate source lookup
+    return None
+
+def import_vote(vote_number):
+    # TODO: parse official vote record
+    pass
+
+def main():
+    latest = get_latest_vote_number()
+    print(f'Latest vote: {latest}')
+
+if __name__ == '__main__':
+    main()
